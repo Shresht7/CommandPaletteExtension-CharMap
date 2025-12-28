8ed476db8c24b7945e1dbc73310b76eb54ed3cb8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-28 14:52:46 +0530

    Create `ISymbol` Interface
    Describes the structure of a `Symbol`

---
+++ CharacterMapExtension/CharMap/ISymbol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterMapExtension.CharMap;
+
+public interface ISymbol
+{
+    string Symbol { get; }
+    string Description { get; }
+    string Category { get; }
+    string Unicode { get; }
+    string Dec { get; }
+    string Latex { get; }
+    List<string> Keywords { get; }
+}
