7c23f3f681a08e4fe7fd3c25fd1848e9dd789f4a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-28 14:57:56 +0530

    Create an Implementation of the `ISymbol` interface for `SymbolData`

---
+++ CharacterMapExtension/CharMap/Symbol.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CharacterMapExtension.CharMap;
+
+internal class SymbolData: ISymbol
+{
+    [JsonPropertyName("symbol")]
+    public string Symbol => string.Empty;
+
+    [JsonPropertyName("description")]
+    public string Description => string.Empty;
+
+    [JsonPropertyName("category")]
+    public string Category => string.Empty;
+
+    [JsonPropertyName("unicode")]
+    public string Unicode => string.Empty;
+
+    [JsonPropertyName("decimal")]
+    public string Dec => string.Empty;
+
+    [JsonPropertyName("latex")]
+    public string Latex => string.Empty;
+
+    [JsonPropertyName("keywords")]
+    public List<string> Keywords => [];
+}
