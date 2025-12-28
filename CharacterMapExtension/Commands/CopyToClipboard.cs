eae8b20156a8f213b4f3bc2a4678282777757ee4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-28 17:51:44 +0530

    Create `CopyToClipboard` Command

---
+++ CharacterMapExtension/Commands/CopyToClipboard.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterMapExtension.Commands
+{
+    internal partial class CopyToClipboard : InvokableCommand
+    {
+        public override string Name => "Copy";
+        public override IconInfo Icon => new("\uE8C8");
+
+        /// <summary>
+        /// The _text to copy to clipboard
+        /// </summary>
+        private readonly string _text = string.Empty;
+
+        public CopyToClipboard(string text)
+        {
+            _text = text;
+        }
+
+        public override ICommandResult Invoke()
+        {
+            ClipboardHelper.SetText(_text);
+            return CommandResult.ShowToast($"Copied {_text} to clipboard");
+        }
+    }
+}
